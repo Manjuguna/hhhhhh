2bba387c5b7b54e3c39b596da0ed58c9f097918d
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-31 15:15:58 +0530

    Create kgf.py

---
+++ kgf.py
@@ -0,0 +1,10 @@
+size8=int(input())
+ara3=[]
+for i in range(size8):
+	sur=input()
+	sur=list(map(int,st.split(" ")))
+	lit=len(sur)
+	for j in range(lit):
+		ara3.append(sur[j])
+ara3.sort()
+print(*ara3,sep=" ")	
